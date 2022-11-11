05299a42511b112fc18d3873d59674a8b4c6a4d4
All <101561325+mritgur@users.noreply.github.com>
2022-11-11 20:58:35 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {  
-  if (document.querySelector('.h-slider')  ) {
+  if (document.querySelector('.h-slider')   ) {
     const slider = tns({
       "container": ".h-slider",
       items: 1,
@@ -12,8 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
       nav: false,
       autoplayButtonOutput	: false ,
     });
-
   }
+  
+  //  if (document.querySelector('.development_projects-slider')   ) {
   const slider = tns({
     "container": ".development_projects-slider",
     items: 1,
@@ -27,6 +28,23 @@ document.addEventListener('DOMContentLoaded', function () {
     autoplayButtonOutput	: false, 
     "mouseDrag": true,
   });
+// } 
+
+//   const zgu_slider = tns({
+//     "container": ".zgu-slider",
+//     items: 1,
+//     // "autoWidth": true,
+//     "swipeAngle": false,
+//     "autoplayTimeout": 5500,
+//     slideBy: '1',
+//     autoplay: true,
+//     controls:false,
+//     navContainer: "#slPager",
+//     autoplayButtonOutput	: false, 
+//     "mouseDrag": true,
+//   });
+// }
+
   const trust_slider = tns({
     "container": ".partners-slider",
     items: 3,
@@ -39,6 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
     autoplayButtonOutput:false,
 
   });
+
 document.querySelector('.prev').addEventListener('click', function () {
   trust_slider.goTo('prev');
   });
@@ -46,9 +65,9 @@ document.querySelector('.next').addEventListener('click', () => {
   trust_slider.goTo('next');
   });
 
-  document.querySelector('.up_link').onclick = () => {
-    window.scrollTo(pageYOffset, 0);
-}
+//   document.querySelector('.up_link').onclick = () => {
+//     window.scrollTo(pageYOffset, 0);
+// }
 
 
 
